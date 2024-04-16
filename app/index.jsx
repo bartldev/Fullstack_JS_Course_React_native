efa7183ca3d5b017b4bfb43005cb2fdf56e4012d
Muxu96 <bartl_fabian@web.de>
2024-04-16 17:37:54 +0200

    initial commit

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hallo Schnuck!</Text>
+      <Text>Hallo Schnucks!</Text>
       <StatusBar style="auto" />
     </View>
   );
