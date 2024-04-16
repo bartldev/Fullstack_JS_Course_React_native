b6859051dc265b9f5a7396c558fdebd4ad849858
Muxu96 <bartl_fabian@web.de>
2024-04-16 22:29:22 +0200

    added native wind - added fonts

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hallo Schnucks!</Text>
+  return ( //minute 30 ca für nativewind
+    <View className="flex-1 items-center justify-center align-center bg-white">
+      <Text className="text-3xl">Hallo Schnucks!</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
